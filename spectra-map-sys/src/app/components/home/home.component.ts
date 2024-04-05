6802baeaaf66f1c50a61679279d3921b1cb7b9aa
Ramiro <rmacias@yps.com.ar>
2024-04-05 09:18:58 -0300

    Cambio en url de reporte powerbi

--- spectra-map-sys/src/app/components/home/home.component.ts
+++ spectra-map-sys/src/app/components/home/home.component.ts
@@ -8,7 +8,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class HomeComponent {
   //urlDelInforme: string = 'https://app.powerbi.com/groups/me/reports/50c9465c-3445-4949-8ded-4cde7950c086/ReportSection?experience=power-bi';
-  urlDelInforme: string = `https://app.powerbi.com/reportEmbed?reportId=50c9465c-3445-4949-8ded-4cde7950c086&autoAuth=true&ctid=4df329da-b94a-4ea1-8013-338f10a162a2`;
+  urlDelInforme: string = `https://app.powerbi.com/reportEmbed?reportId=50c9465c-3445-4949-8ded-4cde7950c086&autoAuth=true&ctid=4df329da-b94a-4ea1-8013-338f10a162a2&amp;rs:embed=true`;
   powerbiReportId: string = "50c9465c-3445-4949-8ded-4cde7950c086";
   @Input() reportUrl!: string;
   safeReportUrl!: SafeResourceUrl;
